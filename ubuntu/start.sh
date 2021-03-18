bdfee67d921942d5b2cc5306d5aec0622737cbbf
Rob Lourens <roblourens@gmail.com>
2021-03-18 09:52:24 -0700

    Add key and start script. Note - Don't get worried about seeing a private key commited in this repo. It's not used for anything but this container. Don't check in your private keys.

--- ubuntu/start.sh
+++ ubuntu/start.sh
@@ -1,5 +1,12 @@
 set -e
 
 docker build -t ssh-ubuntu .
-docker run -dp 1111:22 ssh-ubuntu
-echo "VS Code -> 'Connect To Host' -> root@localhost:1111 -> password is 'root'"
+id=$(docker run -dp 1111:22 ssh-ubuntu)
+if [[ $? == 0 ]]; then
+    echo $id > .current_id
+    echo "Started $id"
+    echo "VS Code -> 'Connect To Host' -> root@localhost:1111 -> password is 'root'"
+else
+    echo "Starting container failed"
+    echo $id
+fi
